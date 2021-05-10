02b6094d73ec3789e2d058ebe8ebf5d3e7ee73ff
Agnes Pious <agnes.pious@capgemini.com>
2021-05-10 21:11:04 +0530

    latest

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const timestamp = require('time-stamp');
 
 exports.handler = function (event, context) {
-    console.log('first build test pipeline');
+    console.log('first build test pipeline latest');
     console.log(JSON.parse(event));
 	context.succeed('hello ' + event.name);
     console.log('timestamp :'+timestamp());
