70b18b8aae107b0add6bca40e246912dcea7ad31
Agnes Pious <agnes.pious@capgemini.com>
2021-05-10 21:25:11 +0530

    new commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const timestamp = require('time-stamp');
 
 exports.handler = function (event, context) {
-    console.log('first build test pipeline latest');
+    console.log('first build test');
     console.log(JSON.parse(event));
-	context.succeed('hello ' + event.name);
+	context.succeed('hello ' + event.key);
     console.log('timestamp :'+timestamp());
 }
