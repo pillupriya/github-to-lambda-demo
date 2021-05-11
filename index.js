391305920366aabd5563921853462e1760d1d117
Agnes Pious <agnes.pious@capgemini.com>
2021-05-11 19:10:05 +0530

    qa

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const timestamp = require('time-stamp');
 
 exports.handler = function (event, context) {
-    console.log('first build test');
+    console.log('first build qa');
     console.log(JSON.parse(event));
 	context.succeed('hello ' + event.key);
     console.log('timestamp :'+timestamp());
